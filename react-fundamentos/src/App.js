9186308dfbb14da57b60d4ade63837b51e818916
gustavocsouza <gustavo.c.souza1991@gmail.com>
2023-08-21 15:46:23 -0300

    prop children, tipando props, props default

--- react-fundamentos/src/App.js
+++ react-fundamentos/src/App.js
@@ -1,28 +1,33 @@
 import React from "react";
 
+import Header from "./Header";
 import Post from "./Post";
 
 function App() {
     return (
-        <>
-            <h1>Component App</h1>
-            <h2>Posts da semana</h2>
+        <>  
+            <Header>
+                <h2>Posts da semana</h2>
+            </Header>
 
             <hr />
 
             <Post
+                likes={20}
                 post={{
                     title: 'titulo01',
                     subtitle: 'subtitulo01'
                 }}
             />
             <Post
+                likes={10}
                 post={{
                     title: 'titulo02',
                     subtitle: 'subtitulo02'
                 }}
             />
             <Post
+                likes={42}
                 post={{
                     title: 'titulo03',
                     subtitle: 'subtitulo03'
--- react-fundamentos/src/Post.js
+++ react-fundamentos/src/Post.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React from 'react';
+import PropTypes from 'prop-types';
 
-function Post({ post }) {
+function Post({ post, likes }) {
     return (
         <>
             <article>
@@ -8,9 +9,19 @@ function Post({ post }) {
                 <br />
                 <small>{ post.subtitle }</small>
             </article>
+            Media: { likes / 2 }
+            <br />
             <br />
         </>
     );
 }
 
 export default Post;
+
+Post.propTypes = {
+    likes: PropTypes.number.isRequired,
+    post: PropTypes.shape({
+        title: PropTypes.string.isRequired,
+        subtitle: PropTypes.string.isRequired,
+    }).isRequired,
+};
