bf82dc9d6a0531a8f1d8a1e418f869d165ef899f
gustavocsouza <gustavo.c.souza1991@gmail.com>
2023-08-29 16:57:24 -0300

    react animation, react router

--- react-fundamentos/src/components/Header/index.js
+++ react-fundamentos/src/components/Header/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 
 import { Container } from './styles';
 
+
 export default function Header({ onToggleTheme, selectedTheme }) {
+  const history = useHistory();
+  console.log(history);
+
+  const handleNavigate = () => {
+    history.push('/posts');
+  }
   return (
     <Container>
       <h1>JStack's Blog</h1>
@@ -12,6 +20,12 @@ export default function Header({ onToggleTheme, selectedTheme }) {
       >
         {selectedTheme === 'dark' ? '🌞' : '🌚'}
       </button>
+      <button
+        style={{
+          color: '#fff'
+        }}  
+        onClick={handleNavigate}
+      >Posts</button>
     </Container>
   );
 }
--- react-fundamentos/src/components/Layout/index.js
+++ react-fundamentos/src/components/Layout/index.js
@@ -1,22 +1,32 @@
 import React from 'react';
+import { Link, BrowserRouter } from 'react-router-dom';
 
 import Routes from '../../Routes';
 
 import Header from '../Header';
 import Footer from '../Footer';
 
+import { Nav } from './styles';
+
 export default function Layout({ onToggleTheme, selectedTheme }) {
   return (
-    <>
+    <BrowserRouter>
       <Header 
         onToggleTheme={onToggleTheme}
         selectedTheme={selectedTheme}
       />
+
+      <Nav>
+        <Link to='/'>Home</Link>
+        <Link to='/posts'>Posts</Link>
+        <Link to='/posts/132131'>Post</Link>
+      </Nav>
+
       <Routes />
-      <Footer 
+      {/* <Footer 
         onToggleTheme={onToggleTheme}
         selectedTheme={selectedTheme}
-      />
-    </>
+      /> */}
+    </BrowserRouter>
   );
 }
